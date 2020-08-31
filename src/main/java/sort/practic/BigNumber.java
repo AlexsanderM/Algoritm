9d0026b52d704045ea575d9fd82baa8bec499e0d
AlexsanderM <malov.alexsander@yandex.ru>
2020-09-01 00:10:38 +0200

    bigNumber decision quicksort

--- src/main/java/sort/practic/BigNumber.java
+++ src/main/java/sort/practic/BigNumber.java
@@ -15,44 +15,90 @@ public class BigNumber {
         List<Integer> listNumber = list.stream().map(Integer::parseInt).collect(Collectors.toList());
 
         List<Integer> listSortNumber = sortArrBigNumber(listNumber, 0, lengthArr - 1);
-        System.out.println(listSortNumber);
+        listSortNumber.forEach(System.out::print);
     }
 
     public static List<Integer> sortArrBigNumber(List<Integer> listNumber, int low, int high) {
-        if (listNumber.size() == 0)
-            return listNumber;
-
-        if (low >= high)
-            return listNumber;
-
-        int middle = low + (high - low) / 2;
-        int opora = listNumber.get(middle);
-
-        int i = low, j = high;
-        while (i <= j) {
-            while (listNumber.get(i) > opora) {
-                i++;
+        int leftMarker = low;
+        int rightMarker = high;
+        int pivot = listNumber.get((leftMarker + rightMarker) / 2);
+        do {
+            while (listNumber.get(leftMarker) != pivot && isEquality(listNumber.get(leftMarker), pivot)) { //*/ (listNumber.get(leftMarker) < pivot) {
+                leftMarker++;
             }
 
-            while (listNumber.get(j) < opora) {
-                j--;
+            while (listNumber.get(rightMarker) != pivot && !isEquality(listNumber.get(rightMarker), pivot)) { //*/ (listNumber.get(rightMarker) > pivot) {
+                rightMarker--;
             }
 
-            if (i <= j) {//меняем местами
-                int temp = listNumber.get(i);
-                listNumber.set(i, listNumber.get(j));
-                listNumber.set(j, temp);
-                i++;
-                j--;
+            if (leftMarker <= rightMarker) {
+                if (leftMarker < rightMarker) {
+                    int tmp = listNumber.get(leftMarker);
+                    listNumber.set(leftMarker, listNumber.get(rightMarker));
+                    listNumber.set(rightMarker, tmp);
+                }
+
+                leftMarker++;
+                rightMarker--;
+            }
+        } while (leftMarker <= rightMarker);
+
+        if (leftMarker < high)
+            sortArrBigNumber(listNumber, leftMarker, high);
+
+        if (low < rightMarker)
+            sortArrBigNumber(listNumber, low, rightMarker);
+
+        return listNumber;
+    }
+
+    private static boolean isEquality(int num, int opora) {
+        int firstNum = num;
+        int secondNum = opora;
+        int firstRaz = 0;
+        int secondRaz = 0;
+
+        while (firstNum >= 10) {
+            firstNum /= 10;
+            firstRaz++;
+        }
+
+        while (secondNum >= 10) {
+            secondNum /= 10;
+            secondRaz++;
+        }
+
+        if (firstNum == secondNum) {
+            if (firstRaz >= 1 && secondRaz >= 1) {
+                num %= (int)Math.pow(10, firstRaz);
+                opora = opora % (int)Math.pow(10, secondRaz);
+
+                return isEquality(num, opora);
+            } else if (firstRaz >= 1) {
+                num %= (int)Math.pow(10, firstRaz);
+                int temp = num;
+
+                while (temp >= 10)
+                    temp /= 10;
+
+                if (temp == opora)
+                    isEquality(num, opora);
+
+                return temp > opora;
+            } else if (secondRaz >= 1) {
+                opora %= (int)Math.pow(10, secondRaz);
+                int temp = num;
+
+                while (temp >= 10)
+                    temp /= 10;
+
+                if (temp == num)
+                    isEquality(num, opora);
+
+                return temp > opora;
             }
         }
 
-        if (low < j)
-            sortArrBigNumber(listNumber, low, j);
-
-        if (high > i)
-            sortArrBigNumber(listNumber, i, high);
-
-        return listNumber;
+        return firstNum > secondNum;
     }
 }
